b10c8e4b3a3bd84a5238b271a82949a2581c886c
Saltolu50 <olaide_salako1@yahoo.com>
2016-12-06 23:06:45 +0100

    Add files via upload

---
+++ NewClass1.java
@@ -0,0 +1,9 @@
+package tutorials;
+public class NewClass1 {
+    
+    public static void main(String args[])
+    {
+        System.out.println("SALAKO OLAIDE TOLULOPE");
+        System.out.println("CSC/2014/0060");
+    }
+}
